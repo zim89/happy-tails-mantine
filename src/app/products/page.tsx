2a8ffa34aa97c052b8bc9c74a505730bcb0f67e2
zim89 <121241841+zim89@users.noreply.github.com>
2023-12-26 15:10:16 +0200

    update Header

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Box, Container } from '@mantine/core';
-import styles from './styles.module.css';
 
 export default function ProductsPage() {
   return (
     <div className='h-screen bg-teal-400'>
       <Container>
-        <Box className={styles.wrap}>xsdbdsfbdfbdfb</Box>
+        <Box>xsdbdsfbdfbdfb</Box>
         <div className='h-12 bg-red-500'></div>
       </Container>
       <div className='container h-12 bg-yellow-500'>
