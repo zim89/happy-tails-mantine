22c79ec80a129b93e40a008da6caccec9b4527d5
nick <nkovlev4@gmail.com>
2023-12-28 13:12:12 +0200

    additional

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
+import AdditionalPage from "@/components/AdditionalPage";
+
 export default function HomePage() {
   return (
-    <>
-      <button className='btn-primary'>Primary</button>
-      <button className='btn-secondary'>Secondary</button>
-      <h2 className='text-2xl font-bold text-red-900'>sfbvsdbvs</h2>
-    </>
+    <div>
+      <AdditionalPage/>
+    </div>
   );
 }
