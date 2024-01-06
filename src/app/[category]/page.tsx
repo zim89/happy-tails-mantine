047fd4df55aef54cda7a38c87f605390ce5812b0
Vladyslav Vladinov <111443297+SalOne22@users.noreply.github.com>
2024-01-06 12:40:05 +0100

    feat: added markdown support

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -1,5 +1,9 @@
 import { getAllCategories, getCategoryByPath } from '@/shared/api/categoryApi';
 import React from 'react';
+import { MDXRemote } from 'next-mdx-remote/rsc';
+
+import styles from './style.module.css';
+import clsx from 'clsx';
 
 export function generateStaticParams() {
   const categories = getAllCategories();
@@ -18,9 +22,18 @@ export default function CatalogPage({
 }) {
   const category = getCategoryByPath(params.category);
 
-  console.log(params);
-
   return (
-    <div className='text-2xl font-bold text-red-900'>{category?.name} page</div>
+    <div className='container text-center'>
+      <h2 className='mb-2 text-[1.75rem]/[normal]'>{category?.title}</h2>
+      <p className='mx-auto mb-8 font-light md:max-w-[28.625rem] lg:max-w-[35.75rem]'>
+        {category?.test_description}
+      </p>
+      <p className='mb-[6.1875rem] text-center font-light text-brand-grey-700 md:mb-36 md:text-2xl/normal'>
+        There are no products in this category yet
+      </p>
+      <div className={clsx('text-left', styles.markdown)}>
+        <MDXRemote source={category?.test_overview ?? ''} />
+      </div>
+    </div>
   );
 }
