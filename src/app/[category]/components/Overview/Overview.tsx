1449012b7cafb97a8eba0f64db08eea529e7ef23
Vladyslav Vladinov <111443297+SalOne22@users.noreply.github.com>
2024-01-06 13:17:03 +0100

    feat: added collapse to overview

---
+++ src/app/[category]/components/Overview/Overview.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import React, { useRef } from 'react';
+import clsx from 'clsx';
+import { useDisclosure } from '@mantine/hooks';
+
+import styles from './style.module.css';
+import { ChevronDown } from 'lucide-react';
+
+export default function Overview({ children }: { children: React.ReactNode }) {
+  const collapse = useRef<HTMLDivElement>(null);
+  const [opened, { toggle }] = useDisclosure(false);
+
+  const onCollapse = () => {
+    if (!collapse.current) return;
+
+    toggle();
+    const totalHeight = collapse.current.scrollHeight;
+
+    collapse.current.style.maxHeight = `${opened ? 430 : totalHeight}px`;
+  };
+
+  return (
+    <div className={clsx('text-left', styles.markdown)}>
+      <div
+        ref={collapse}
+        className='relative max-h-[26.875rem] overflow-hidden transition-[max-height] duration-500'
+      >
+        <div
+          className={clsx(
+            'absolute bottom-0 h-[15.3125rem] w-screen bg-[linear-gradient(356deg,_#fdfdfd_29.88%,_transparent_92.68%)] transition-opacity duration-500 md:h-[13.6875rem] lg:h-[10.375rem]',
+            opened && 'opacity-0'
+          )}
+        ></div>
+        {children}
+      </div>
+      <button
+        className='mt-2 w-36 py-2.5 text-left font-bold'
+        onClick={onCollapse}
+      >
+        {opened ? 'See less' : 'Show more'}
+        <ChevronDown
+          className={clsx('ml-2 inline', opened && '-scale-y-100')}
+        />
+      </button>
+    </div>
+  );
+}
--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -1,9 +1,8 @@
 import { getAllCategories, getCategoryByPath } from '@/shared/api/categoryApi';
 import React from 'react';
-import { MDXRemote } from 'next-mdx-remote/rsc';
 
-import styles from './style.module.css';
-import clsx from 'clsx';
+import Overview from './components/Overview';
+import { MDXRemote } from 'next-mdx-remote/rsc';
 
 export function generateStaticParams() {
   const categories = getAllCategories();
@@ -31,9 +30,10 @@ export default function CatalogPage({
       <p className='mb-[6.1875rem] text-center font-light text-brand-grey-700 md:mb-36 md:text-2xl/normal'>
         There are no products in this category yet
       </p>
-      <div className={clsx('text-left', styles.markdown)}>
+
+      <Overview>
         <MDXRemote source={category?.test_overview ?? ''} />
-      </div>
+      </Overview>
     </div>
   );
 }
