78ead12332d60d0ae4bd2912e275be4079626ea5
Vladyslav Vladinov <111443297+SalOne22@users.noreply.github.com>
2024-01-16 13:15:36 +0100

    feat: added unstyled filter

--- src/modules/Toolbar/Toolbar.tsx
+++ src/modules/Toolbar/Toolbar.tsx
@@ -1,6 +1,14 @@
-import { Category } from '@/shared/api/categoryApi';
+'use client';
+
+import { Category, getAllCategories } from '@/shared/api/categoryApi';
 import SortBy, { type Option } from './components/SortBy';
 import Filter from './components/Filter';
+import { useDisclosure, useMediaQuery } from '@mantine/hooks';
+import { useForm } from '@mantine/form';
+import FilterForm from './components/FilterForm';
+import { FilterFormValues } from './components/FilterForm/FilterForm';
+import { Collapse } from '@mantine/core';
+import { useId, useRef } from 'react';
 
 const sortOptions: Option[] = [
   { title: 'Featured', value: 'featured' },
@@ -10,12 +18,40 @@ const sortOptions: Option[] = [
   { title: 'Alphabetically, Z - A', value: 'name-des' },
 ];
 
-export default function Toolbar({ category }: { category: Category }) {
+export type ToolbarProps = {
+  category: Category;
+};
+
+export default function Toolbar({ category }: ToolbarProps) {
+  const collapseId = useId();
+
+  const form = useForm<FilterFormValues>({
+    initialValues: {
+      categories: [],
+      prices: [],
+      onlyInStock: false,
+    },
+  });
+
+  const categories = getAllCategories();
+
   return (
-    <div className='mb-4 flex items-center text-sm/4 md:py-2 md:text-base'>
-      <Filter />
-      <p className='hidden md:block'>{category?.productCount} Results</p>
-      <SortBy options={sortOptions} />
+    <div>
+      <div className='mb-4 flex items-center text-sm/4 md:py-2 md:text-base'>
+        <Filter
+          target={`#${CSS.escape(collapseId)}`}
+          form={form}
+          category={category}
+          categories={categories}
+          onSubmit={form.onSubmit((values) => console.log(values))}
+        />
+        <p className='hidden md:block'>{category?.productCount} Results</p>
+        <SortBy
+          options={sortOptions}
+          onSelect={(option) => console.log(option)}
+        />
+      </div>
+      <div id={collapseId}></div>
     </div>
   );
 }
--- src/modules/Toolbar/components/Filter/Filter.tsx
+++ src/modules/Toolbar/components/Filter/Filter.tsx
@@ -1,29 +1,43 @@
 'use client';
 
-import { Drawer } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
-import { Plus, X } from 'lucide-react';
+import { Collapse, Drawer, Portal } from '@mantine/core';
+import { Plus } from 'lucide-react';
+import FilterForm, { FilterFormProps } from '../FilterForm/FilterForm';
+import { useDisclosure, useMediaQuery } from '@mantine/hooks';
 
-export default function Filter() {
-  const [opened, { open, close }] = useDisclosure(false);
+export type FilterProps = { target: string } & FilterFormProps;
+
+export default function Filter({ target, ...props }: FilterProps) {
+  const [opened, { close, toggle }] = useDisclosure(false);
+  const isDesktop = useMediaQuery('(min-width: 1280px)') ?? false;
 
   return (
     <>
       <Drawer
-        opened={opened}
+        opened={opened && !isDesktop}
         onClose={close}
         title='Filter'
         classNames={{
           inner: 'max-w-[22.375rem] md:max-w-[30.9375rem]',
           header: 'bg-brand-grey-300 p-4 h-[4.625rem]',
           title: 'text-xl font-bold',
+          body: 'p-4 md:px-9 md:py-8',
         }}
         closeButtonProps={{
           iconSize: '100%',
         }}
-      ></Drawer>
+      >
+        <FilterForm {...props} onClose={close} />
+      </Drawer>
+      <Portal target={target}>
+        <Collapse in={opened && isDesktop} transitionDuration={500}>
+          <div className='relative rounded-sm border border-brand-grey-400 p-7 pb-[4.625rem]'>
+            <FilterForm {...props} desktop onClose={close} />
+          </div>
+        </Collapse>
+      </Portal>
       <button
-        onClick={open}
+        onClick={toggle}
         className='flex h-[2.375rem] w-full items-center justify-center gap-2 rounded-sm border border-brand-grey-300 bg-brand-grey-300 px-[1.125rem] font-bold md:mr-6 md:max-w-[10rem] lg:mr-12'
       >
         Filter
