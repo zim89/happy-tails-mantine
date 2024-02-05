2a17ff9852f5d2ff778f3548ae0781e1e75e37a7
zim89 <121241841+zim89@users.noreply.github.com>
2024-02-05 08:33:38 +0200

    Update loading.tsx

--- src/app/blog/[id]/loading.tsx
+++ src/app/blog/[id]/loading.tsx
@@ -3,7 +3,7 @@ import { Loader } from '@mantine/core';
 export default function Loading() {
   return (
     <div className='flex h-full items-center justify-center'>
-      <Loader color='orange' size={36} />
+      <Loader color='orange' size={32} />
     </div>
   );
 }
