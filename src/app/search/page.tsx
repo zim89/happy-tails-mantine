bb79dffa83643fa09e06f4bcf22a2dc85e18f456
zim89 <121241841+zim89@users.noreply.github.com>
2024-02-15 17:23:20 +0200

    Update page.tsx
    add Toolbar on search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Container, TextInput } from '@mantine/core';
 import { useDebouncedValue } from '@mantine/hooks';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
@@ -7,6 +7,32 @@ import { Search, XCircle } from 'lucide-react';
 
 import Breadcrumbs from '@/components/Breadcrumbs';
 import CatalogProductList from '@/modules/CatalogProductList';
+import { Category, getAllCategories } from '@/shared/api/categoryApi';
+import Toolbar from '@/modules/Toolbar';
+import ProductCountContextProvider from '@/modules/CatalogProductList/ProductCountContext';
+
+const category: Category = {
+  id: 0,
+  name: 'All Products',
+  title: 'Premium Dog Products',
+  description:
+    'We understand that your furry friend deserves nothing but the best. Discover a delightful array of high-quality products designed to enhance your dog&apos;s comfort, happiness, and well-being',
+  overview: `From cozy beds to stylish apparel, interactive toys to nutritious treats, our curated collection offers a wide range of items to cater to your dog's every need. Each product is thoughtfully selected to ensure your canine companion experiences ultimate joy and care. Experience the joy of pampering your beloved companion with the finest dog products available. Our commitment to quality ensures that your dog receives the care they truly deserve. Explore our catalog now and elevate your dog's lifestyle to new heights of comfort and happiness!
+### Comfortable Rest
+Explore our luxurious dog beds and cozy sleeping options that provide your pup with the perfect place to unwind after a day of play. <ins>Plush Bolster Bed for Small Breeds, Cozy Donut Cuddler Bed</ins> - our beds provide optimal support and comfort, ensuring your canine companion enjoys restful sleep
+### Tail-Wagging Fashion
+Dress your furry friend in style with our trendy dog apparel, from adorable sweaters to practical raincoats, making walks and outings a fashion statement. Waterproof raincoats, doggie bandanas in various prints, adorable dog bowties or reflective safety vests for night walks our clother collection combines fashion and function!
+### Engaging Playtime
+Choose from an assortment of interactive toys that keep them entertained for hours on end.
+### Grooming and Wellness
+Discover grooming products that promote your dog's hygiene and well-being, from gentle shampoos to effective tick and flea solutions.
+### Travel Essentials
+Make adventures with your dog stress-free with our travel-friendly accessories, including collapsible bowls, comfortable carriers, and more.
+### Stylish Collars and Durable Leads
+Ensure your dog's safety and style during walks with our exquisite collection of collars and leads. Choose from a range of designs and materials that blend fashion and functionality seamlessly.`,
+  path: 'products',
+  productCount: 0,
+};
 
 interface Props {
   searchParams: {
@@ -20,21 +46,22 @@ export default function Page({ searchParams }: Props) {
   const path = usePathname();
   const { replace } = useRouter();
 
-  const [value, setValue] = useState(searchParams.name);
-  const [debounced] = useDebouncedValue(value, 300);
+  const [inputValue, setInputValue] = useState(searchParams.name);
+  const [debounced] = useDebouncedValue(inputValue, 300);
+  const [categories, setCategories] = useState<Category[]>([]);
 
   const onChange = (value: string) => {
     const params = new URLSearchParams(query);
     params.set('page', '1');
     replace(`${path}?${params.toString()}`);
-    setValue(value);
+    setInputValue(value);
   };
 
   const onReset = () => {
     const params = new URLSearchParams(query);
     params.set('page', '1');
     replace(`${path}?${params.toString()}`);
-    setValue('');
+    setInputValue('');
   };
 
   useEffect(() => {
@@ -42,6 +69,13 @@ export default function Page({ searchParams }: Props) {
     debounced ? params.set('name', debounced) : params.delete('name');
     replace(`${path}?${params.toString()}`);
   }, [debounced, path, query, replace]);
+
+  useEffect(() => {
+    (async () => {
+      const { content: categories } = await getAllCategories();
+      setCategories(categories);
+    })();
+  }, []);
 
   return (
     <Container>
@@ -56,7 +90,7 @@ export default function Page({ searchParams }: Props) {
             <TextInput
               placeholder='What are you looking for?'
               leftSection={<Search className='h-4 w-4' />}
-              value={value}
+              value={inputValue}
               onChange={(event) => onChange(event.currentTarget.value)}
               classNames={{
                 input:
@@ -64,7 +98,7 @@ export default function Page({ searchParams }: Props) {
                 section: 'text-brand-grey-600',
               }}
             />
-            {value && (
+            {inputValue && (
               <button
                 className='group absolute right-3 top-1/2 -translate-y-1/2'
                 onClick={onReset}
@@ -74,7 +108,11 @@ export default function Page({ searchParams }: Props) {
             )}
           </div>
         </div>
-        <CatalogProductList />
+
+        <ProductCountContextProvider>
+          <Toolbar category={category} categories={categories} />
+          <CatalogProductList />
+        </ProductCountContextProvider>
       </div>
     </Container>
   );
