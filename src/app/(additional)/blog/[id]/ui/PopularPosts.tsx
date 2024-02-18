40ab43b0d183013438f0af7e29088f603f57ac5a
zim89 <121241841+zim89@users.noreply.github.com>
2024-02-18 10:45:11 +0200

    Update PopularPosts.tsx
    add link to post details on click

--- src/app/(additional)/blog/[id]/ui/PopularPosts.tsx
+++ src/app/(additional)/blog/[id]/ui/PopularPosts.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import { fetchLastFivePosts } from '@/shared/api/postApi';
 import { formatDate } from '@/shared/lib/helpers';
 import noImage from '@/assets/icons/no-image.512x512.png';
+import Link from 'next/link';
 
 async function PopularPosts() {
   const posts = await fetchLastFivePosts();
@@ -14,31 +15,36 @@ async function PopularPosts() {
 
       <ul className='grid grid-cols-1 gap-6 lg:gap-8'>
         {posts.map((post) => (
-          <li key={post.id} className='group flex cursor-pointer gap-4'>
-            <div className='relative h-[120px] w-[120px] flex-none'>
-              <Image
-                src={
-                  post.posterImgSrc.includes('http')
-                    ? post.posterImgSrc
-                    : noImage
-                }
-                alt={post.title}
-                fill={true}
-                sizes='120px'
-                className='object-cover'
-              />
-            </div>
-
-            <div className='flex items-center'>
-              <div className='flex flex-col gap-2'>
-                <h3 className='text-lg/[1.3] text-secondary transition-colors duration-300 group-hover:text-brand-orange-400'>
-                  {post.title}
-                </h3>
-                <p className='text-base font-light'>
-                  {formatDate(post.createdAt)}
-                </p>
+          <li key={post.id}>
+            <Link
+              href={`/blog/${post.id}`}
+              className='group flex cursor-pointer gap-4'
+            >
+              <div className='relative h-[120px] w-[120px] flex-none'>
+                <Image
+                  src={
+                    post.posterImgSrc.includes('http')
+                      ? post.posterImgSrc
+                      : noImage
+                  }
+                  alt={post.title}
+                  fill={true}
+                  sizes='120px'
+                  className='object-cover'
+                />
               </div>
-            </div>
+
+              <div className='flex items-center'>
+                <div className='flex flex-col gap-2'>
+                  <h3 className='text-lg/[1.3] text-secondary transition-colors duration-300 group-hover:text-brand-orange-400'>
+                    {post.title}
+                  </h3>
+                  <p className='text-base font-light'>
+                    {formatDate(post.createdAt)}
+                  </p>
+                </div>
+              </div>
+            </Link>
           </li>
         ))}
       </ul>
