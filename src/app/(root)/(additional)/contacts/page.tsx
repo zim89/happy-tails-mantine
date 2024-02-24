396d9cfe1e7bc9d931f5794f66095841f6f72ee5
Dima Honcharenko <d.goncharenko.man@gmail.com>
2024-02-24 14:21:23 +0200

    Auxiliary adjustments: returned .env.example, replaced apostrophes to its encoded variant

--- src/app/(root)/(additional)/contacts/page.tsx
+++ src/app/(root)/(additional)/contacts/page.tsx
@@ -75,11 +75,11 @@ export default function ContactsPage() {
       />
       <div className={styles.content}>
         <hgroup className={styles.heading}>
-          <h1>We'd love to hear from you!</h1>
+          <h1>{"We'd love to hear from you!"}</h1>
           <p>
             Whether you have questions about our products, need assistance with
             an order, or just want to share stories about your furry friend,
-            we're here to help.
+            we&apos;re here to help.
           </p>
         </hgroup>
         <form
@@ -153,7 +153,7 @@ export default function ContactsPage() {
 
         <p className={styles.partner_message}>
           For partnership opportunities, collaborations, or wholesale inquiries,
-          please email us at partnerships@happytails.com. We're always open to
+          please email us at partnerships@happytails.com. We&apos;re always open to
           new and exciting ventures.
         </p>
       </div>
