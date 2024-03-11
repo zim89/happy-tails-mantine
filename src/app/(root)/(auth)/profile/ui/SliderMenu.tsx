ee2095f5e4a404f475fc269a519ddee323dd13d9
Dima Honcharenko <d.goncharenko.man@gmail.com>
2024-03-11 08:59:59 +0200

    Removed timeout ref

--- src/app/(root)/(auth)/profile/ui/SliderMenu.tsx
+++ src/app/(root)/(auth)/profile/ui/SliderMenu.tsx
@@ -1,24 +1,25 @@
 'use client';
 
-import { profileMenu } from "@/modules/Header/lib/data";
-import Link from "next/link";
-import { useCallback, useEffect, useRef, useState, UIEvent } from "react";
+import { profileMenu } from '@/modules/Header/lib/data';
+import Link from 'next/link';
+import { useCallback, useEffect, useRef, useState, UIEvent } from 'react';
 
-import classes from "./style.module.css";
+import classes from './style.module.css';
 
 let lastScrollLeft = 0;
 let direction = 1;
+let timeout: number;
 
 export default function SliderMenu() {
-    const [isScrolling, setIsScrolling] = useState(false);
-    let timeout = useRef<number>(null);
-    let transform = useRef<HTMLDivElement>(null);
+  const [isScrolling, setIsScrolling] = useState(false);
+  let transform = useRef<HTMLDivElement>(null);
 
-    const handleScroll = useCallback((e: UIEvent<HTMLDivElement>) => {
-      if (timeout.current) {
-        clearTimeout(timeout.current);
+  const handleScroll = useCallback(
+    (e: UIEvent<HTMLDivElement>) => {
+      if (timeout) {
+        clearTimeout(timeout);
       }
-      
+
       if (e.currentTarget.scrollLeft > lastScrollLeft) {
         direction = 1;
       } else {
@@ -28,36 +29,40 @@ export default function SliderMenu() {
       lastScrollLeft = e.currentTarget.scrollLeft;
 
       setIsScrolling(true);
-  
-      timeout.current = setTimeout(() => {
+
+      timeout = window.setTimeout(() => {
         setIsScrolling(false);
       }, 1000);
-    }, [timeout.current])
-    
+    },
+    [timeout]
+  );
 
-    useEffect(() => {
-      return () => {
-        if (timeout.current) {
-          clearTimeout(timeout.current);
-        }
-      }
-    }, []);
+  useEffect(() => {
+    return () => {
+      window.clearTimeout(timeout);
+    };
+  }, []);
 
-    return (
-
-        <div className='relative custom-scroll sm: overflow-hidden overflow-x-scroll min-w-max h-16 bg-slate-200 md:hidden' onScroll={(e) => handleScroll(e)}>
-        <div ref={transform} className={`${classes.menu} ${isScrolling ? classes.paused : ''}`}>
-          {profileMenu.map((item) => (
-            <Link
-              key={item.id}
-              href={item.href}
-              className='flex gap-2 whitespace-nowrap bg-slate-200 p-4'
-            >
-              <item.icon />
-              <span>{item.label}</span>
-            </Link>
-          ))}
-        </div>
+  return (
+    <div
+      className='custom-scroll sm: relative h-16 min-w-max overflow-hidden overflow-x-scroll bg-slate-200 md:hidden'
+      onScroll={(e) => handleScroll(e)}
+    >
+      <div
+        ref={transform}
+        className={`${classes.menu} ${isScrolling ? classes.paused : ''}`}
+      >
+        {profileMenu.map((item) => (
+          <Link
+            key={item.id}
+            href={item.href}
+            className='flex gap-2 whitespace-nowrap bg-slate-200 p-4'
+          >
+            <item.icon />
+            <span>{item.label}</span>
+          </Link>
+        ))}
       </div>
-    )
+    </div>
+  );
 }
