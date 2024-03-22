dd80f202d0e90cb30d411554dff5a22f74b36d82
Dima Honcharenko <d.goncharenko.man@gmail.com>
2024-03-22 11:01:38 +0200

    Hid sins in contact page + added thank-page

--- src/app/(root)/profile/page.tsx
+++ src/app/(root)/profile/page.tsx
@@ -14,7 +14,7 @@ export default function ProfilePage() {
   return (
     <div className='mx-auto flex flex-col md:max-w-[572px]'>
       <hgroup className='text-center'>
-        <h1 className='text-[28px]'>My Personal Account</h1>
+        <h1 className='heading'>My Personal Account</h1>
         <p className='py-4 font-light'>
           Hello Diana, welcome to your Happy Tails account. Here you can manage
           your details, repeat orders and view your order history.
