fbdeab48b7fd8ff662c781b04a1772cc962ac6dd
Dima Honcharenko <d.goncharenko.man@gmail.com>
2024-04-16 09:41:01 +0300

    Added change order status (UI)

--- src/modules/OrderTable/components/UpdateStatus.tsx
+++ src/modules/OrderTable/components/UpdateStatus.tsx
@@ -19,8 +19,8 @@ export default function UpdateStatus({ children, orderRow }: Props) {
   };
 
   const close = () => {
-      setOpened(false);
-    };
+    setOpened(false);
+  };
 
   return (
     <Popover
@@ -33,23 +33,39 @@ export default function UpdateStatus({ children, orderRow }: Props) {
       shadow='md'
     >
       <Popover.Target>{children(toggle)}</Popover.Target>
-      <Popover.Dropdown classNames={{ dropdown: 'flex gap-4' }}>
-        <Select
-          value={selectedOption}
-          classNames={{
-            input: "form-input"
-          }}
-          onChange={(option) => setSelectedOption((prev) => option || prev)}
-          data={['SHIPPED', 'IN PROGRESS', 'NEW', 'CANCELLED', 'COMPLETED', 'RETURN PROCESSING', 'PROCESSING']}
-          rightSection={<ChevronDown size={16} color="black" />}
-        />
-        <div>
-          <Button classNames={{ root: 'bg-black mr-2' }}>
-            <Check size={16} />
-          </Button>
-          <Button classNames={{ root: 'border-[1px] border-[#EEE]' }} onClick={close}>
-            <X size={16} color='black' />
-          </Button>
+      <Popover.Dropdown classNames={{ dropdown: "p-0" }}>
+        <div className="bg-[#EEEEEE] text-[#787878] text-sm font-black uppercase px-2 py-1">
+            Edit
+        </div>
+        <div className='flex gap-4 py-4 px-2'>
+          <Select
+            value={selectedOption}
+            classNames={{
+              input: 'form-input',
+            }}
+            onChange={(option) => setSelectedOption((prev) => option || prev)}
+            data={[
+              'SHIPPED',
+              'IN PROGRESS',
+              'NEW',
+              'CANCELLED',
+              'COMPLETED',
+              'RETURN PROCESSING',
+              'PROCESSING',
+            ]}
+            rightSection={<ChevronDown size={16} color='black' />}
+          />
+          <div>
+            <Button classNames={{ root: 'bg-black mr-2' }}>
+              <Check size={16} />
+            </Button>
+            <Button
+              classNames={{ root: 'border-[1px] border-[#EEE]' }}
+              onClick={close}
+            >
+              <X size={16} color='black' />
+            </Button>
+          </div>
         </div>
       </Popover.Dropdown>
     </Popover>
