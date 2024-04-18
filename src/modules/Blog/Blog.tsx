2cca0697cb87fd64f5e900fd0165c72a20533ed1
Vladyslav Vladinov <vladinov.dev@gmail.com>
2024-04-18 16:15:09 +0200

    fix: fixed post path

--- src/modules/Blog/Blog.tsx
+++ src/modules/Blog/Blog.tsx
@@ -12,15 +12,8 @@ export default async function Blog() {
         Blog
       </h2>
 
-      <Container className='flex flex-col items-center'>
+      <Container>
         <PostList posts={posts} />
-        <Button
-          component={Link}
-          href='/blog'
-          className='mt-6 h-11 min-w-full rounded-sm border border-brand-grey-400 bg-brand-grey-100 text-[hsl(0,0%,9%)] md:mt-9 md:min-w-[232px]'
-        >
-          Read more
-        </Button>
       </Container>
     </section>
   );
