3230dd09b75fd67463db2ebe5ffb35a8ddef7af1
unknown <d.goncharenko.man@gmail.com>
2024-08-18 19:32:45 +0300

    Added test cases for AddToWishBtn

--- src/components/TestWrapper.tsx
+++ src/components/TestWrapper.tsx
@@ -1,16 +1,14 @@
-import { AppProviders } from "@/shared/config/AppProviders"
-import theme from "@/shared/config/theme"
-import { MantineProvider } from "@mantine/core"
+import { AppProviders } from '@/shared/config/AppProviders';
+import theme from '@/shared/config/theme';
+import { MantineProvider } from '@mantine/core';
 
 type Props = {
-    children: React.ReactNode;
-}
+  children: React.ReactNode;
+};
 export const TestWrapper = ({ children }: Props) => {
-    return (
-        <AppProviders>
-            <MantineProvider theme={theme}>
-                {children}
-            </MantineProvider>
-        </AppProviders>
-    )
-}
+  return (
+    <AppProviders>
+      <MantineProvider theme={theme}>{children}</MantineProvider>
+    </AppProviders>
+  );
+};
