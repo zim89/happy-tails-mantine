65b45f346d07963ad3e8409ccf0084ec21ee3587
unknown <d.goncharenko.man@gmail.com>
2024-09-17 22:38:59 +0300

    Removed unused code

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -1,20 +1,14 @@
 import BarChart from '@/modules/BarChart';
-import LineChart from '@/modules/LineChart';
 import OrdersChart from '@/modules/OrdersChart';
 import Stats from '@/modules/Stats';
 import TopCategories from '@/modules/TopCategories';
-import { auth } from '../../../../auth';
-
 export default async function Page() {
-  const session = await auth();
-
   return (
     <div className='flex flex-col gap-6'>
       <Stats />
       <BarChart />
       <OrdersChart />
       <TopCategories />
-      {session && <LineChart />}
     </div>
   );
 }
