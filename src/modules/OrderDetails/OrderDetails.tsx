9a88881797fd8e0bf73a65d7c9d21474a7333ac2
unknown <d.goncharenko.man@gmail.com>
2024-10-17 15:24:45 +0300

    Cleared arbitrary console logs

--- src/modules/OrderDetails/OrderDetails.tsx
+++ src/modules/OrderDetails/OrderDetails.tsx
@@ -7,19 +7,12 @@ import { ProductTable } from './components/ProductTable';
 import { ShippingDetails } from './components/ShippingDetails';
 import { ClientDetails } from './components/ClientDetails';
 import { CommentSection } from './components/CommentSection';
-import {
-  useFindOneQuery,
-  useFindOneByEmailAndCodeQuery,
-} from '@/shared/api/ordersApi';
+import { useFindOneByEmailAndCodeQuery } from '@/shared/api/ordersApi';
 import Loader from '@/components/Loader';
 
 export default function OrdersDetails() {
   const { id } = useParams();
-  console.log('ID: ', decodeURIComponent(`${id}`));
-
   const [number, email] = decodeURIComponent(`${id}`).split('^');
-
-  console.log('Email-Code', number, email);
 
   const { data, error, isLoading } = useFindOneByEmailAndCodeQuery({
     orderNumber: number,
