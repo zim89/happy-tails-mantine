58eedd67d3ed60cf4561349e2fa65cc240cafc51
Dima Honcharenko <d.goncharenko.man@gmail.com>
2024-11-07 18:06:08 +0200

    Added experimental bot assistance

---
+++ src/modules/ChatBot/ChatBot.tsx
@@ -0,0 +1,100 @@
+'use client';
+
+import axios from 'axios';
+import Bot from 'react-chatbotify';
+import { BotIcon } from 'lucide-react';
+
+import classes from './classes.module.css';
+import Image from 'next/image';
+
+export const ChatBot = () => {
+  let hasError = false;
+
+  const call_chat_service = async (params: any) => {
+    try {
+      const res = await axios.get(
+        `http://localhost:3000/api/assistance?message=${params.userInput}`
+      );
+
+      await params.injectMessage(res.data);
+    } catch (err) {
+      console.error(err);
+      await params.injectMessage(
+        'Unable to load model, is your API Key valid?'
+      );
+      hasError = true;
+    }
+  };
+
+  const flow = {
+    start: {
+      message: 'Greetings 😊, how would I help you?',
+      path: 'loop',
+    },
+
+    loop: {
+      message: async (params: any) => {
+        await call_chat_service(params);
+      },
+      path: () => {
+        if (hasError) {
+          return 'start';
+        }
+        return 'loop';
+      },
+    },
+  };
+
+  return (
+    <div className={classes.chat}>
+      <Bot
+        settings={{
+          botBubble: { dangerouslySetInnerHtml: true },
+          chatHistory: { autoLoad: true, storageKey: 'chat_bot' },
+          general: {
+            fontFamily: '__Inter_90234e',
+            secondaryColor: '#DB8420',
+            primaryColor: '#F39324',
+          },
+          notification: {
+            disabled: true,
+          },
+          header: {
+            title: (
+              <h2 className='mt-1 text-lg font-semibold'>
+                Happy Tails Assistant
+              </h2>
+            ),
+            avatar:
+              'https://happy-tails-mantine.vercel.app/logo/logo-mobile.svg',
+          },
+          fileAttachment: {
+            disabled: true,
+          },
+          footer: {
+            text: (
+              <p className='mb-2 text-brand-grey-600'>
+                Be specific to be comprehensive to the bot
+              </p>
+            ),
+          },
+          chatButton: {
+            icon: () => (
+              <Image
+                src='/images/bot_dog.png'
+                height={48}
+                width={48}
+                className='invert filter'
+                alt='Chat bot'
+              />
+            ),
+          },
+          tooltip: {
+            mode: 'hidden',
+          },
+        }}
+        flow={flow}
+      />
+    </div>
+  );
+};
--- src/modules/Stats/Stats.tsx
+++ src/modules/Stats/Stats.tsx
@@ -15,43 +15,45 @@ export default function Stats() {
   if (!values || isLoading) return <SkeletonLoader />;
 
   return (
-    <div className='flex gap-2'>
-      {data.map((stat, index) => (
-        <div
-          key={index}
-          className='w-full rounded-l bg-white p-4 shadow-[0px_4px_8px_0px_#0000000F,0px_4px_4px_0px_#0000000A]'
-        >
-          <div className='flex items-center justify-between gap-6'>
-            <div>
-              <p className='whitespace-nowrap text-lg text-brand-grey-400'>
-                {stat.name}
-              </p>
-              <p className='mt-1 text-[1.75rem] font-bold'>
-                {stat.type === 'currency' ? (
-                  <NumberFormatter
-                    prefix='$'
-                    decimalScale={2}
-                    className='whitespace-nowrap pl-2'
-                    value={values[stat.property]}
-                    thousandSeparator=','
-                  />
-                ) : (
-                  values[stat.property]
-                )}
-              </p>
+    <>
+      <div className='flex gap-2'>
+        {data.map((stat, index) => (
+          <div
+            key={index}
+            className='w-full rounded-l bg-white p-4 shadow-[0px_4px_8px_0px_#0000000F,0px_4px_4px_0px_#0000000A]'
+          >
+            <div className='flex items-center justify-between gap-6'>
+              <div>
+                <p className='whitespace-nowrap text-lg text-brand-grey-400'>
+                  {stat.name}
+                </p>
+                <p className='mt-1 text-[1.75rem] font-bold'>
+                  {stat.type === 'currency' ? (
+                    <NumberFormatter
+                      prefix='$'
+                      decimalScale={2}
+                      className='whitespace-nowrap pl-2'
+                      value={values[stat.property]}
+                      thousandSeparator=','
+                    />
+                  ) : (
+                    values[stat.property]
+                  )}
+                </p>
+              </div>
+              <span className='flex h-12 w-12 items-center justify-center rounded-full bg-primary'>
+                <stat.icon color='#F39324' size={24} />
+              </span>
             </div>
-            <span className='flex h-12 w-12 items-center justify-center rounded-full bg-primary'>
-              <stat.icon color='#F39324' size={24} />
-            </span>
+            {values[stat.propertyInWeek] > 0 && (
+              <p className='mt-4 flex max-w-max items-center gap-2 rounded-lg bg-brand-green-100 px-2 py-[5px] text-brand-green-400'>
+                <TrendingUp strokeWidth='3px' size={16} />
+                <span>+{values[stat.propertyInWeek]} this week</span>
+              </p>
+            )}
           </div>
-          {values[stat.propertyInWeek] > 0 && (
-            <p className='mt-4 flex max-w-max items-center gap-2 rounded-lg bg-brand-green-100 px-2 py-[5px] text-brand-green-400'>
-              <TrendingUp strokeWidth='3px' size={16} />
-              <span>+{values[stat.propertyInWeek]} this week</span>
-            </p>
-          )}
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </>
   );
 }
