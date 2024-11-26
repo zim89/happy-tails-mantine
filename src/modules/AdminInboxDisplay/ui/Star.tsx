e8061764da7d6feb8fec9ecebd521a8b7500e926
zim89 <121241841+zim89@users.noreply.github.com>
2024-11-26 16:59:29 +0200

    #375 - messages in admin

--- src/modules/AdminInboxDisplay/ui/Star.tsx
+++ src/modules/AdminInboxDisplay/ui/Star.tsx
@@ -1,34 +1,44 @@
-import { UnstyledButton } from '@mantine/core';
+import { useToggleStarredMutation } from '@/shared/api/feedbackApi';
+import { isAxiosQueryError, isErrorDataString } from '@/shared/lib/helpers';
+import { Loader, UnstyledButton } from '@mantine/core';
 import { LucideStar } from 'lucide-react';
-import { Dispatch, SetStateAction } from 'react';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
 
-export const Star = ({
-  id,
-  starred,
-  setStarred,
-}: {
-  id: number;
-  starred: boolean;
-  setStarred?: Dispatch<SetStateAction<number[]>>;
-}) => {
-  const isStarred = starred === true;
-  //   const star = (id: number) => {
-  //     setStarred((prev) => [...prev, id]);
-  //   };
+export const Star = ({ id, starred }: { id: number; starred: boolean }) => {
+  const [loading, setLoading] = useState(false);
+  const [isStarred, setIsStarred] = useState(starred);
+  const [toggleStarred] = useToggleStarredMutation();
 
-  //   const unstar = (id: number) => {
-  //     setStarred((prev) => prev.filter((candidate) => candidate !== id));
-  //   };
+  const handleToggleStarred = async (id: number) => {
+    try {
+      setLoading(true);
+      await toggleStarred(id).unwrap();
+      setIsStarred(!isStarred);
+    } catch (err) {
+      if (isAxiosQueryError(err)) {
+        toast.error(isErrorDataString(err.data) ? err.data : err.data.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  if (loading) {
+    return (
+      <UnstyledButton>
+        <Loader size={16} color='black' />
+      </UnstyledButton>
+    );
+  }
 
   return (
-    <UnstyledButton
-    //   onClick={starred ? unstar.bind(null, id) : star.bind(null, id)}
-    >
-      {isStarred ? (
-        <LucideStar stroke='#FBBC04' fill='#FBBC04' size={16} />
-      ) : (
-        <LucideStar size={16} />
-      )}
+    <UnstyledButton onClick={() => handleToggleStarred(id)}>
+      <LucideStar
+        stroke={isStarred ? '#FBBC04' : 'black'}
+        fill={isStarred ? '#FBBC04' : 'none'}
+        size={16}
+      />
     </UnstyledButton>
   );
 };
